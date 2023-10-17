c64cf0f9b59cef72f9cacf48c9bb2e97073cc0a3
QuentinPlatiau <quentin.platiau@student.vinci.be>
2023-10-17 12:44:04 +0200

    change activedTimer

--- exercices/ex2.4.1/index.js
+++ exercices/ex2.4.1/index.js
@@ -2,8 +2,6 @@ const clickTimer = document.querySelector("#msgBox1");
 const buttonClick = document.querySelector("#click");
 
 clickTimer.addEventListener("mouseover", activedTimer);
-buttonClick.addEventListener("click", compteurDeClick);
-
 
 let timeoutID;
 let StartGame;
@@ -11,12 +9,18 @@ let compteur = 0;
 const delayInSeconds = 5;
 const delayInMiliSeconds = delayInSeconds * 1000;
 
-function activedTimer() {    
-    clickTimer.removeEventListener("mouseover",activedTimer);
-    StartGame = new Date();
-    timeoutID = setTimeout(() => {
-        alert('Game over, you did not click 10 times within 5s !');
+function activedTimer() {
+  if(!timeoutID){    
+      buttonClick.addEventListener("click", compteurDeClick);    
+      clickTimer.removeEventListener("mouseover",activedTimer);
+      StartGame = new Date();
+      timeoutID = setTimeout(() => {
+      timeoutID = undefined;
+      compteur = 0;
+      clickTimer.addEventListener("mouseover", activedTimer);
+      alert('Game over, you did not click 10 times within 5s !');
       }, delayInMiliSeconds);
+    }
 }
 
 function compteurDeClick(){
