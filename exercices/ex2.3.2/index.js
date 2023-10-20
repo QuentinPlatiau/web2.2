cd2df5f7aeef0a93ba26aff27c9626afaefd2b81
QuentinPlatiau <quentin.platiau@student.vinci.be>
2023-10-20 15:21:24 +0200

    finish

--- exercices/ex2.3.2/index.js
+++ exercices/ex2.3.2/index.js
@@ -2,10 +2,12 @@ const divs = document.querySelectorAll("div");
 
 divs.forEach((div) => {
   div.addEventListener("mouseover", () => {
-    div.innerText = `${div.style.backgroundColor.toString()}`;
+      div.className = "color-div2"
+      div.innerText = `${div.style.backgroundColor.toString()}`;
   });
 
   div.addEventListener("mouseout", () => {
+    div.className = "color-div"
     div.innerText = "";
   });
 });
