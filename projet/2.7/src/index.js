c6bc93eb0189452520cd1d62fdcdde6eaa7d905b
QuentinPlatiau <quentin.platiau@student.vinci.be>
2023-10-27 13:51:57 +0200

    test grille jeu

--- projet/2.7/src/index.js
+++ projet/2.7/src/index.js
@@ -1,2 +1,41 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+// eslint-disable-next-line prefer-const
+let grille = [
+    ["tour","cavalier","fou","dame","roi","fou","cavalier","tour"],
+    ["pion","pion","pion","pion","pion","pion","pion","pion"],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["pion","pion","pion","pion","pion","pion","pion","pion"],
+    ["tour","cavalier","fou","roi","dame","fou","cavalier","tour"]
+];
+
+const divs = document.querySelectorAll("div");
+
+let val = "";
+let id = 0;
+
+divs.forEach((div) => {
+    div.addEventListener("click", () => {
+        if(val === ""){        
+            console.log(grille);
+            val =  `${div.innerText}`;
+            id = div.id;
+        }
+
+        else if(val !== ""){
+            if(id !== div.id){
+                // eslint-disable-next-line no-param-reassign
+                div.innerHTML = val;
+                grille[Math.floor((div.id-1)/8)][(div.id-1)%8] = val;
+                val = "";
+                divs.item(id-1).innerText = "";            
+                grille[Math.floor((id-1)/8)][(id-1)%8] = "";
+                console.log(grille);
+            }
+        }
+    });
+});
